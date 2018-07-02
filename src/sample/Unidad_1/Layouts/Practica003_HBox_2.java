1576084540ebf271bbc7f11bfc9bafeac2801c19
Roberto Loaeza Valerio <roberto.loaeza@gmail.com>
2018-07-02 12:09:51 -0500

    Samples of Layouts

---
+++ src/sample/Unidad_1/Layouts/Practica003_HBox_2.java
@@ -0,0 +1,37 @@
+package sample.Unidad_1.Layouts;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Practica003_HBox_2 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage escenarioPrincipal) {
+        Button boton = new Button("Click aquí");
+        Label etiqueta = new Label("Hola mundo");
+        Rectangle rectangulo = new Rectangle(150,200, Color.AQUAMARINE);
+
+        HBox contenedor = new HBox(10);
+        contenedor.setPadding(new Insets(20));
+        contenedor.getChildren().add(boton);
+        contenedor.getChildren().add(rectangulo);
+        contenedor.getChildren().add(etiqueta);
+
+
+
+        Scene escena = new Scene(contenedor);
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.show();
+    }
+}
---
+++ src/sample/Unidad_1/Layouts/Practica004_BorderPane_1.java
@@ -0,0 +1,44 @@
+package sample.Unidad_1.Layouts;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Practica004_BorderPane_1 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage escenarioPrincipal) {
+        Button bArriba = new Button("Arriba");
+        Button bAbajo = new Button("Abajo");
+        Button bIzquierda = new Button("Izquierda");
+        Button bDerecha = new Button("Derecha");
+        Button bCentro = new Button("Centro");
+
+        BorderPane contenedor = new BorderPane();
+
+        contenedor.setBottom(bAbajo);
+        contenedor.setTop(bArriba);
+        contenedor.setLeft(bIzquierda);
+        contenedor.setRight(bDerecha);
+        contenedor.setCenter(bCentro);
+
+        BorderPane.setAlignment(bArriba, Pos.CENTER);
+
+
+
+        Scene escena = new Scene(contenedor,300, 150);
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.show();
+    }
+}
