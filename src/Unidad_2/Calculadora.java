4419b0ac6c573670b39871123e4358599039b13c
Roberto Loaeza Valerio <roberto.loaeza@gmail.com>
2018-07-23 07:20:09 -0500

    Working with ScheneBuilder

--- src/Unidad_2/Calculadora.java
+++ src/Unidad_2/Calculadora.java
@@ -10,7 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+
 
 public class Calculadora extends Application {
 
