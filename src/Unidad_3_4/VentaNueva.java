495c64e84a3dcd6d3bcb1f7ec6cf99454dc7cda0
Roberto Loaeza Valerio <roberto.loaeza@gmail.com>
2018-07-31 11:17:29 -0500

    PuntoDeVenta

--- src/Unidad_3_4/VentaNueva.java
+++ src/Unidad_3_4/VentaNueva.java
@@ -6,16 +6,13 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.sql.*;
@@ -24,9 +21,20 @@ import java.util.ResourceBundle;
 
 public class VentaNueva implements Initializable {
 
-    private ArrayList<VentaListaProductos> productos;
-    private ObservableList<Venta> elementosTabla;
     private ArrayList<Cliente> clientes;
+
+    private ArrayList<VentaProductoCosto> ventaProductoCostos;
+
+    private ObservableList<VentaElementos> ventaElementos;
+
+    private double calcularTotal() {
+        double total = 0;
+        
+        for(VentaElementos v : ventaElementos) {
+            total = total + v.getCostoT();
+        }
+        return total;
+    }
 
 
     @FXML
@@ -36,180 +44,143 @@ public class VentaNueva implements Initializable {
     private JFXTextField producto;
 
     @FXML
-    private JFXComboBox<String> cliente;
-
-    @FXML
     private JFXListView<String> listaProductos;
 
     @FXML
     private JFXTextField cantidad;
 
     @FXML
-    private TableView<Venta> tabla;
+    private TableView<VentaElementos> tabla;
 
     @FXML
     private Label granTotal;
 
-    private double calcularTotal () {
-        double total = 0;
-        for(Venta v : elementosTabla) {
-            total += v.getCosto()*v.getCantidad();
-        }
-        return total;
-    }
+    @FXML
+    private JFXComboBox<String> cliente;
 
     @FXML
     void AgregarProducto(ActionEvent event) {
-        int indiceLista = listaProductos.getSelectionModel().getSelectedIndex();
-        if(indiceLista<0)
-            return;
-        int idProducto = productos.get(indiceLista).getIdProducto();
-        int idExistencia =productos.get(indiceLista).getIdExistencia();
-        double costo =productos.get(indiceLista).getCosto();
-        String nombreProducto = productos.get(indiceLista).getNombre();
+        ventaElementos.add(new VentaElementos(1,1, 20, "Producto X", 20));
+    }
 
+    @FXML
+    void Cancelar(ActionEvent event) {
 
-        int cantidadProducto = cantidad.getText().isEmpty()?1:Integer.valueOf(cantidad.getText());
+    }
 
-        elementosTabla.add(new Venta(idExistencia, idProducto, cantidadProducto, nombreProducto, costo));
+    @FXML
+    void Guardar(ActionEvent event) {
 
-        granTotal.setText(String.valueOf(calcularTotal()));
-
-        producto.setText("");
-        listaProductos.getItems().clear();
-        productos.clear();
     }
 
     @FXML
     void Quitar(ActionEvent event) {
         int indice = tabla.getSelectionModel().getSelectedIndex();
-        if(indice<0)
+        if( indice<0 )
             return;
-        elementosTabla.remove(indice);
+
+        ventaElementos.remove(indice);
 
         granTotal.setText(String.valueOf(calcularTotal()));
-
-    }
-    @FXML
-    void Cancelar(ActionEvent event) {
-        Pane p = (Pane)contenedor.getParent();
-        p.getChildren().remove(0);
-    }
-
-    @FXML
-    void Guardar(ActionEvent event) throws SQLException {
-
-            int idCliente = clientes.get(cliente.getSelectionModel().getSelectedIndex()).getIdCliente();
-            double total = calcularTotal();
-
-            int idVenta=-1;
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:pventa.db");
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(60);
-
-            String sql = "INSERT INTO ventas(idCliente, fecha, total) VALUES(" +
-                    idCliente+ "," +
-                    "date('now'), "+
-                    total+")";
-            statement.execute(sql);
-
-            sql = "SELECT last_insert_rowid()";
-
-            ResultSet resultSet= statement.executeQuery(sql);
-            if(resultSet.next()) {
-                idVenta = resultSet.getInt(1);
-            }
-
-            for(Venta v: elementosTabla) {
-                sql = "INSERT INTO VentasDetalle(idVenta, idProducto, costo) VALUES (" + idVenta+", "+v.getIdProducto()+", "+v.getCosto()+")";
-                statement.execute(sql);
-            }
-
-            producto.clear();
-            listaProductos.getItems().clear();
-            productos.clear();
-            elementosTabla.clear();
-            granTotal.setText("0.0");
 
 
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        producto.setOnKeyTyped(e->{
-            String patron = producto.getText();
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:pventa.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(60);
 
-            if(patron.length()>3) {
-                try {
-                    Connection connection = DriverManager.getConnection("jdbc:sqlite:pventa.db");
-                    Statement statement = connection.createStatement();
-                    statement.setQueryTimeout(60);
-                    String sql = "SELECT * FROM productos, existencias WHERE productos.idProducto=existencias.idProducto AND ( nombre LIKE '%"+patron+"%' OR descripcion LIKE '%"+patron+"%' )";
-                    productos = new ArrayList<VentaListaProductos>();
-                    listaProductos.getItems().clear();
-                    ResultSet resultSet = statement.executeQuery(sql);
-                    while(resultSet.next()) {
-                        listaProductos.getItems().add(resultSet.getString("nombre"));
+            String sql = "SELECT * FROM clientes";
+            ResultSet resultSet = statement.executeQuery(sql);
 
-                        productos.add(new VentaListaProductos(
-                                resultSet.getInt("idProducto"),
-                                resultSet.getInt("idProveedor"),
-                                resultSet.getString("nombre"),
-                                resultSet.getString("descripcion"),
-                                resultSet.getInt("idExistencia"),
-                                resultSet.getInt("cantidad"),
-                                resultSet.getDouble("costo")
-                                ));
-                    }
-                    sql = "SELECT * FROM clientes";
+            clientes = new ArrayList<Cliente>();
+            cliente.getItems().clear();
 
-                    resultSet = statement.executeQuery(sql);
-
-                    clientes = new ArrayList<Cliente>();
-                    cliente.getItems().clear();
-                    while(resultSet.next()) {
-                        clientes.add(new Cliente(
-                                resultSet.getInt("idCliente"),
-                                resultSet.getString("nombre"),
-                                resultSet.getString("rfc"),
-                                resultSet.getString("calle"),
-                                resultSet.getString("colonia"),
-                                resultSet.getString("ciudad"),
-                                resultSet.getString("pais"),
-                                resultSet.getString("telefono"),
-                                resultSet.getString("celular"),
-                                resultSet.getString("email")
-                        ));
-                        cliente.getItems().add(
-                                resultSet.getString("nombre"));
-
-                    }
-
-                } catch (SQLException e1) {
-                    e1.printStackTrace();
-                }
+            while(resultSet.next()) {
+                cliente.getItems().add(resultSet.getString("nombre"));
+                clientes.add(new Cliente(
+                        resultSet.getInt("idCliente"),
+                        resultSet.getString("nombre"),
+                        resultSet.getString("rfc"),
+                        resultSet.getString("calle"),
+                        resultSet.getString("colonia"),
+                        resultSet.getString("ciudad"),
+                        resultSet.getString("pais"),
+                        resultSet.getString("telefono"),
+                        resultSet.getString("celular"),
+                        resultSet.getString("email")
+                ));
 
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        producto.setOnKeyTyped(e-> {
+            String patron = producto.getText();
+            if( patron.length()<3 )
+                return;
+
+
+            try {
+                Connection connection = DriverManager.getConnection("jdbc:sqlite:pventa.db");
+                Statement statement = connection.createStatement();
+
+                String sql = "SELECT * FROM productos, existencias " +
+                        "WHERE productos.idProducto=existencias.idProducto " +
+                        "AND nombre LIKE '%" + patron + "%' " +
+                        "AND cantidad > 0";
+
+                ResultSet resultSet = statement.executeQuery(sql);
+                ventaProductoCostos = new ArrayList<VentaProductoCosto>();
+                listaProductos.getItems().clear();
+                while(resultSet.next()) {
+                    listaProductos.getItems().add(
+                            resultSet.getString("nombre"));
+
+                    ventaProductoCostos.add(new VentaProductoCosto(
+                       resultSet.getInt("idProducto"),
+                        resultSet.getInt("idExistencia"),
+                        resultSet.getString("nombre"),
+                        resultSet.getString("descripcion"),
+                        resultSet.getInt("cantidad"),
+                        resultSet.getDouble("costo")
+                    ));
+
+                }
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+
+
         });
 
-
-        TableColumn<Venta, String> columnaCantidad= new TableColumn<>("Cantidad");
-        columnaCantidad.setMinWidth(20);
+        TableColumn<VentaElementos, String> columnaCantidad = new TableColumn<>("Cant.");
+        columnaCantidad.setMinWidth(10);
         columnaCantidad.setCellValueFactory(new PropertyValueFactory<>("cantidad"));
 
-        TableColumn<Venta, String> columnaProducto= new TableColumn<>("Producto");
-        columnaProducto.setMinWidth(300);
-        columnaProducto.setCellValueFactory(new PropertyValueFactory<>("producto"));
+        TableColumn<VentaElementos, String> columnaNombre = new TableColumn<>("Nombre Producto");
+        columnaNombre.setMinWidth(250);
+        columnaNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
 
-        TableColumn<Venta, String> columnaCosto= new TableColumn<>("Costo");
-        columnaCosto.setMinWidth(50);
-        columnaCosto.setCellValueFactory(new PropertyValueFactory<>("costo"));
+        TableColumn<VentaElementos, String> columnaCostoU = new TableColumn<>("C. U.");
+        columnaCostoU.setMinWidth(10);
+        columnaCostoU.setCellValueFactory(new PropertyValueFactory<>("costoU"));
 
+        TableColumn<VentaElementos, String> columnaCostoT = new TableColumn<>("C. T.");
+        columnaCostoT.setMinWidth(10);
+        columnaCostoT.setCellValueFactory(new PropertyValueFactory<>("costoT"));
 
-        elementosTabla = FXCollections.observableArrayList();
-        tabla.setItems(elementosTabla);
         tabla.getColumns().clear();
-        tabla.getColumns().addAll(columnaCantidad, columnaProducto, columnaCosto);
+        tabla.getColumns().addAll(columnaCantidad, columnaNombre, columnaCostoU, columnaCostoT);
+
+
+        ventaElementos = FXCollections.observableArrayList();
+        tabla.setItems(ventaElementos);
+
 
 
     }
--- src/Unidad_3_4/VentaProductoCosto.java
+++ src/Unidad_3_4/VentaProductoCosto.java
@@ -1,20 +1,18 @@
 package Unidad_3_4;
 
-public class VentaListaProductos {
+public class VentaProductoCosto {
     private int idProducto;
-    private int idProveedor;
+    private int idExistencia;
     private String nombre;
     private String descripcion;
-    private int idExistencia;
     private int cantidad;
     private double costo;
 
-    public VentaListaProductos(int idProducto, int idProveedor, String nombre, String descripcion, int idExistencia, int cantidad, double costo) {
+    public VentaProductoCosto(int idProducto, int idExistencia, String nombre, String descripcion, int cantidad, double costo) {
         this.idProducto = idProducto;
-        this.idProveedor = idProveedor;
+        this.idExistencia = idExistencia;
         this.nombre = nombre;
         this.descripcion = descripcion;
-        this.idExistencia = idExistencia;
         this.cantidad = cantidad;
         this.costo = costo;
     }
@@ -27,12 +25,12 @@ public class VentaListaProductos {
         this.idProducto = idProducto;
     }
 
-    public int getIdProveedor() {
-        return idProveedor;
+    public int getIdExistencia() {
+        return idExistencia;
     }
 
-    public void setIdProveedor(int idProveedor) {
-        this.idProveedor = idProveedor;
+    public void setIdExistencia(int idExistencia) {
+        this.idExistencia = idExistencia;
     }
 
     public String getNombre() {
@@ -49,14 +47,6 @@ public class VentaListaProductos {
 
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
-    }
-
-    public int getIdExistencia() {
-        return idExistencia;
-    }
-
-    public void setIdExistencia(int idExistencia) {
-        this.idExistencia = idExistencia;
     }
 
     public int getCantidad() {
